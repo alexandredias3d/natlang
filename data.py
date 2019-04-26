a07da158ee4c43acbc02db6f86c57d05dedb9637
alexandredias3d <alehenriquedias@gmail.com>
2019-04-25 21:20:38 -0300

    Create data.py
    Helper script to download data from Dropbox and uncompress it (if Python was compiled with zlib).

---
+++ data.py
@@ -0,0 +1,43 @@
+import os
+import tarfile
+import wget
+
+br_reviews_url = 'https://www.dropbox.com/s/y6u7rl44nqdoznl/br.tar.gz?dl=0'
+en_reviews_url = 'https://www.dropbox.com/s/wboo4atryaw19bv/en.tar.gz?dl=0'
+
+folder = '../dat'
+
+def make_folder():
+    '''
+        Create the dat folder one directory above.
+    '''
+    os.makedirs(folder, exist_ok=True)
+
+def download_data():
+    '''
+        Download a subset of data to be used during the meetings of the group.
+    '''
+    wget.download(br_reviews_url, out=f'{folder}/br.tar.gz')
+    wget.download(en_reviews_url, out=f'{folder}/en.tar.gz')
+        
+def extract_data():
+    '''
+        Extract the compressed .tar.gz.
+    '''
+    files = [file for file in os.listdir(folder) if file.endswith('.tar.gz')]
+    for file in files:
+        try:
+            tarfile.open(f'{folder}/{file}').extractall()
+            _.close()
+        except tarfile.ReadError:
+            print('\nThis Python wasn\'t compiled with zlib support. Please, '
+                  'extract the data manually.')
+        
+def prepare():
+    '''
+        Do everything that is needed to prepare the data.
+    '''
+    make_folder()
+    download_data()
+    extract_data()
+    
