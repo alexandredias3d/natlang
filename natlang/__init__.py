9fd3497bbb0d97ad4ae67b99fb39b487f43406b9
alexandredias3d <alehenriquedias@gmail.com>
2019-08-08 23:50:26 -0300

    Close #12
    Refactor the postagging module.

---
+++ natlang/__init__.py
@@ -0,0 +1,3 @@
+from natlang.pos import *
+
+
---
+++ natlang/pos/__init__.py
@@ -0,0 +1,14 @@
+from natlang.pos.corpora import *
+from natlang.pos.corpus import *
+from natlang.pos.portuguese import *
+from natlang.pos.tagger import *
+
+__all__ = [
+    'Corpora',
+    'Corpus',
+    'Floresta',
+    'LacioWeb',
+    'MacMorpho',
+    'Tagger',
+    'TaggerBuilder',
+]
